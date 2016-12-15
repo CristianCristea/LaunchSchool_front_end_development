aa16a815fc4dde16870208ac10002f21d21814a3
Cristian Cristea <cristiancristea.1987@gmail.com>
2016-12-15 09:07:17 +0100

    Correct typos

--- jQuery and the DOM/arithmetic_calculator/arithmetic_calculator.js
+++ jQuery and the DOM/arithmetic_calculator/arithmetic_calculator.js
@@ -14,28 +14,31 @@ $(function() {
   var $p = $('#display');
   var result;
 
-$('form').on('submit', function(e) {
-  e.preventDefault();
+  $('form').on('submit', function(e) {
+    e.preventDefault();
 
-  $first_number = Number($('#first_number').val());
-  $second_number = Number($('#second_number').val());
-  $operator = $('select').val();
-  console.log($first_number, $second_number, $operator);
-  switch ($operator) {
-    case 'add':
-      result = $first_number + $second_number;
-      break;
-    case 'subtract':
-      result = $first_number - $second_number;
-      break;
-    case 'multiply':
-      result = $first_number * $second_number;
-      break;
-    case 'divide':
-      result = $first_number / $second_number;
-      break;
-  }
-  console.log(result);
+    $first_number = Number($('#first_number').val());
+    $second_number = Number($('#second_number').val());
+    $operator = $('select').val();
+
+    // console.log($first_number, $second_number, $operator);
+    
+    switch ($operator) {
+      case 'add':
+        result = $first_number + $second_number;
+        break;
+      case 'subtract':
+        result = $first_number - $second_number;
+        break;
+      case 'multiply':
+        result = $first_number * $second_number;
+        break;
+      case 'divide':
+        result = $first_number / $second_number;
+        break;
+    }
+    
+    // console.log(result);
     $p.text(result);
   });
 });
