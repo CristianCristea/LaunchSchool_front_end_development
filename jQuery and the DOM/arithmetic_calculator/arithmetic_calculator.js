00b2622ad90f7705556b1d5ec07bc3e29ce34d60
Cristian Cristea <cristiancristea.1987@gmail.com>
2016-12-15 09:00:43 +0100

    Finish arithmetic_calculator_assignment

---
+++ jQuery and the DOM/arithmetic_calculator/arithmetic_calculator.js
@@ -0,0 +1,41 @@
+// get first number
+// get second number
+// get operator
+// calculate
+// print to page
+  // get the p tag
+  // change the content
+
+
+$(function() {
+  var $first_number;
+  var $second_number;
+  var $operator;
+  var $p = $('#display');
+  var result;
+
+$('form').on('submit', function(e) {
+  e.preventDefault();
+
+  $first_number = Number($('#first_number').val());
+  $second_number = Number($('#second_number').val());
+  $operator = $('select').val();
+  console.log($first_number, $second_number, $operator);
+  switch ($operator) {
+    case 'add':
+      result = $first_number + $second_number;
+      break;
+    case 'subtract':
+      result = $first_number - $second_number;
+      break;
+    case 'multiply':
+      result = $first_number * $second_number;
+      break;
+    case 'divide':
+      result = $first_number / $second_number;
+      break;
+  }
+  console.log(result);
+    $p.text(result);
+  });
+});
