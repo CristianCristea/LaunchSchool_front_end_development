04ab829a92176cfbc816ea687c88a0836a87d04f
Cristian Cristea <cristiancristea.1987@gmail.com>
2016-12-23 19:40:52 +0100

    Refactor HTML, CSS and JS

--- jQuery and the DOM/photo_gallery_slideshow/slideshow.js
+++ jQuery and the DOM/photo_gallery_slideshow/slideshow.js
@@ -1,8 +1,18 @@
 $(function() {
-  $('#thumbnails img').on('click', function() {
-    var index = $(this).index();
+  var $slideshow = $('#slideshow');
+  var $nav = $slideshow.find('ul');
 
-    $(this).addClass('active').siblings().removeClass('active');
-    $('#photos img').eq(index).fadeIn(400).siblings().fadeOut(400);
+  $nav.on('click', 'a', function(e) {
+    e.preventDefault();
+
+    var $li= $(e.currentTarget).closest('li');
+    var index = $li.index();
+
+    $nav.find('li').filter('.active').removeClass('active');
+    $li.addClass('active');
+
+    $slideshow.find('figure')
+      .filter(':visible').stop().fadeOut(300).end()
+      .eq(index).delay(300).fadeIn(300);
   });
 });
