9b603e2ea7528a3b3c764d617114ec39a39abd1c
Cristian Cristea <cristiancristea.1987@gmail.com>
2016-12-23 21:44:59 +0100

    Finish photo_gallery_slideshow_assignment

--- jQuery and the DOM/photo_gallery_slideshow/slideshow.js
+++ jQuery and the DOM/photo_gallery_slideshow/slideshow.js
@@ -1,18 +1,20 @@
 $(function() {
   var $slideshow = $('#slideshow');
   var $nav = $slideshow.find('ul');
+  var $controls = $slideshow.find('.controls');
 
   $nav.on('click', 'a', function(e) {
     e.preventDefault();
 
     var $li= $(e.currentTarget).closest('li');
-    var index = $li.index();
+    var currentIndex = $li.index();
 
-    $nav.find('li').filter('.active').removeClass('active');
+    $nav.find('.active').removeClass('active');
     $li.addClass('active');
 
-    $slideshow.find('figure')
-      .filter(':visible').stop().fadeOut(300).end()
-      .eq(index).delay(300).fadeIn(300);
+      $slideshow.find('figure')
+      .filter(':visible').stop(true, true).fadeOut(300).end()
+      .eq(currentIndex).delay(300).fadeIn(300);
   });
 });
+
